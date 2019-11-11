e7c8d7b95ffb7a834bc3d8c8468f6d64fba6dc13
phatdroid94 <phatdroid94@gmail.com>
2019-11-11 19:26:06 +0700

    pagerAdapter

--- app/src/main/java/com/example/tablayout29072019/TabLayoutPager.java
+++ app/src/main/java/com/example/tablayout29072019/TabLayoutPager.java
@@ -1,24 +1,41 @@
 package com.example.tablayout29072019;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import java.util.ArrayList;
+
 public class TabLayoutPager extends FragmentPagerAdapter {
+
+    private FragmentManager fragmentManager = null;
+    private ArrayList<String> arrayListTitle = new ArrayList<>();
 
     public TabLayoutPager(@NonNull FragmentManager fm) {
         super(fm);
+        fragmentManager = fm;
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return null;
+        return fragmentManager.getFragments().get(position);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return fragmentManager.getFragments().size();
+    }
+
+    public void addArrayTitleFragment(String titleFragment){
+        arrayListTitle.add(titleFragment);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return arrayListTitle.get(position);
     }
 }
