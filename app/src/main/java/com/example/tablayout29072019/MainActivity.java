796cb0850ba377eb8158461e4f5fa9e2c4406e71
phatdroid94 <phatdroid94@gmail.com>
2019-11-11 19:13:14 +0700

    screen fragment

--- app/src/main/java/com/example/tablayout29072019/MainActivity.java
+++ app/src/main/java/com/example/tablayout29072019/MainActivity.java
@@ -18,19 +18,20 @@ public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     MaterialToolbar materialToolbar;
     ViewPager viewPager;
-    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         materialToolbar = findViewById(R.id.materialToolbar);
         viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tablayout);
 
         setSupportActionBar(materialToolbar);
         materialToolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
         getSupportActionBar().setTitle("Main Activity");
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setIcon(android.R.drawable.ic_menu_sort_by_size);
 
     }
 }
