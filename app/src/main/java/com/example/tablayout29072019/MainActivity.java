5d6fb45b67979d4112a157028448f0ab0b998ca8
phatdroid94 <phatdroid94@gmail.com>
2019-11-11 18:56:26 +0700

    background toolbar

--- app/src/main/java/com/example/tablayout29072019/MainActivity.java
+++ app/src/main/java/com/example/tablayout29072019/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.tablayout29072019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
+    MaterialToolbar materialToolbar;
+    ViewPager viewPager;
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        materialToolbar = findViewById(R.id.materialToolbar);
+        viewPager = findViewById(R.id.viewpager);
+
+        setSupportActionBar(materialToolbar);
+        materialToolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        getSupportActionBar().setTitle("Main Activity");
+
     }
 }
