9936360d88b17cc0234d171c881e1dc2b5b3f017
phatdroid94 <phatdroid94@gmail.com>
2019-11-11 18:39:29 +0700

    material toolbar

--- app/src/main/java/com/example/tablayout29072019/MainActivity.java
+++ app/src/main/java/com/example/tablayout29072019/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
